d226628378b227a0302da9b9e63c2646c822a9c9
Ramez Gerges <ramezgerges@tutanota.com>
2020-09-21 16:08:50 +0200

    Courseinfo exercises (1.1 to 1.5) are done.

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => <Part part={part} />)}
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts.reduce((acc, cur) => acc + cur.exercises, 0)}</p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
