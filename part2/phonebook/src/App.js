ad51cefe084496a97a69d26fa5dab4f9778438d0
Ramez Gerges <ramezgerges@tutanota.com>
2020-09-24 12:32:28 +0200

    Phonebook exercise (2.11) is done. Renamed components/FilteredComponents.js to components/FilteredPersons.js.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,24 @@
 /* eslint react/prop-types: 0 */
 //TODO props validation
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import FilteredPersons from "./components/FilteredComponents";
+import FilteredPersons from "./components/FilteredPersons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [ persons, setPersons ] = useState([]);
   const [ newName, setNewName ] = useState("");
   const [ newNumber, setNewNumber ] = useState("");
   const [ nameFilter, setNameFilter ] = useState("");
+
+  useEffect(() =>
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response => setPersons(response.data)))
+  , []);
 
   return (
     <div>
